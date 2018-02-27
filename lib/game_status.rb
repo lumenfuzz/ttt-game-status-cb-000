9e0ac48a03d54f260193fb84c77e15cd77c3b315
Patrick James Louis <lumenfuzz@gmail.com>
2018-02-27 21:52:21 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,19 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+def won?(board)
+  dummy = WIN_COMBINATIONS.each do |win|
+    nil
+  end
+end
